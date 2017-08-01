dc50336061cffcda3b79a515046f429c651d1b7e
sbal13 <sbal13@gmail.com>
2017-08-01 09:18:10 -0400

    Added Session controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :reservations
   resources :users
+
+  get '/', to: "sessions#welcome", as: "welcome"
+  get '/signin', to: "sessions#new", as: "signin"
+  post '/', to: "sessions#create", as: "login"
+  delete '/', to: "sessions#destroy", as: "logout"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
