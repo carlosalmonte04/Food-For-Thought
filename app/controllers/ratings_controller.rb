c6c1e816cbc58a56fd56865a419e86ab68698cd1
carlosalmonte04 <vacarlos_alm@hotmail.com>
2017-08-03 14:54:18 -0400

    from carlos computer

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,29 @@
 class RatingsController < ApplicationController
+
+	def new
+		@tutor = User.find_by(id: params[:button])
+		@reservation_id = params[:review][:reservation_id]
+	end
+
+	def create
+		@rating = Rating.new(value: rating_params[:value], comment: rating_params[:comment])
+		@reservation = Reservation.find_by(id: rating_params[:reservation_id])
+		@rating.reservation = @reservation
+		@tutor = User.find_by(id: rating_params[:tutor_id])
+byebug
+		if @rating.save
+			@reservation.update(status: "reviewed")
+			flash[:message] = "Successfully submitted rating"
+			redirect_to posts_path
+		else
+			flash[:message] = "Please choose one of the values below"
+			render :new
+		end
+	end
+
+	private
+
+	def rating_params
+		params.require(:rating).permit(:value, :comment, :tutor_id, :reservation_id)
+	end
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,4 @@
 class Rating < ApplicationRecord
   belongs_to :reservation
+  validates :value, presence: true
 end
